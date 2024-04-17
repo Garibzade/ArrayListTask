6a9d7f38caf329ce20c4bca21ad311ff9bf8eb36
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-18 02:19:35 +0400

    Added Task

---
+++ ArrayListTask/Models/IntArrayList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListTask.Models
+{
+    internal class IntArrayList
+    {
+        int[] _arr;
+        public int Capacity { get; set; }
+        public int Count { get; private set; }
+        public IntArrayList()
+        {
+            Capacity = 3;
+
+            _arr = new int[Capacity];
+        }
+        public IntArrayList(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new int[Capacity];
+        }
+        public int this[int num]
+        {
+            get { return _arr[Count]; }
+            set { _arr[Count] = value; }
+
+        }
+        public void Add(int item)
+        {
+            if (_arr.Length == Count)
+            {
+                Array.Resize(ref _arr, Capacity + Count);
+            }
+            _arr[Count] = item;
+            Count++;
+
+        }
+        public void GetInfo()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+        public void IndexOf(int value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_arr[i] == value)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+
+
+        }
+        public void LastIndexOf(int value2)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                if (_arr[i] == value2)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+            }
+
+
+        }
+        public void RemoveAt(int index)
+        {
+            _arr[index] = 0;
+        }
+
+
+      public override string ToString()
+        {
+            string result = "{";
+            for (int i = 0; i < Count; i++)
+            {
+                if (i > 0)
+                {
+                    result += ", ";
+                }
+                result += _arr[i].ToString();
+
+            }
+            result += "}";
+            return result;
+        }
+    }
+}
+
+
---
+++ ArrayListTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using ArrayListTask.Models;
+
+namespace ArrayListTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           IntArrayList arrayList = new IntArrayList();
+            arrayList.Add(12);
+            arrayList.Add(23);
+            arrayList.Add(36);
+            arrayList.Add(48);
+            arrayList.Add(59);
+            arrayList.Add(13);
+            arrayList.Add(78);
+            arrayList.Add(45);
+            arrayList.Add(64);
+            //Console.WriteLine(arrayList.ToString());
+            //arrayList.RemoveAt(2);
+
+
+
+            //arrayList.IndexOf(59);
+            //arrayList.LastIndexOf(59);
+            arrayList.RemoveAt(0);
+
+
+
+            arrayList.GetInfo();
+        }
+    }
+}
